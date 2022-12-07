d969da54ffba267f955f4c6f7c5d658fc4fd265c
jalp21@icloud.com <jalp21@icloud.com>
2022-12-06 21:12:54 -0800

    i think empty array not working

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-const methodOverride = require('method-override')
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
@@ -14,7 +15,10 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+  
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
@@ -23,15 +27,17 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-  
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
 
 // 404 Page
 app.get('*', (req, res) => {
     res.send('404')
   })
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
+
+
   
 
