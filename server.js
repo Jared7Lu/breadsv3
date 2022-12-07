8aab261682147dfbdc23b601304fc57a7f239e62
jalp21@icloud.com <jalp21@icloud.com>
2022-12-06 20:30:59 -0800

    everything works

---
+++ server.js
@@ -0,0 +1,37 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+const methodOverride = require('method-override')
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+  
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+
